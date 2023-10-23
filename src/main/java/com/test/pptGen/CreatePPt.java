5ed6c5bd40eda51405ee05e23a17348b177ccc64
Raushan132 <raushankumar132abc@gmail.com>
2023-10-23 07:08:51 +0530

    create table inside ppt

--- src/main/java/com/test/pptGen/CreatePPt.java
+++ src/main/java/com/test/pptGen/CreatePPt.java
@@ -1,5 +1,7 @@
 package com.test.pptGen;
 
+import java.awt.Rectangle;
+import java.awt.geom.Rectangle2D;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 
@@ -16,33 +18,37 @@ public class CreatePPt {
 	
 	 public void getPpt() throws Exception {
 		 
-		 FileInputStream fis = new FileInputStream("C:\\Users\\LENOVO\\Pictures\\powerPoint\\test1.pptx");
-		 XMLSlideShow slideShow = new XMLSlideShow(fis);
+//		 FileInputStream fis = new FileInputStream("C:\\Users\\LENOVO\\Pictures\\powerPoint\\test.pptx");
+		 XMLSlideShow slideShow = new XMLSlideShow();
 	       
 		 var slide =  slideShow.createSlide();
 		 var slide2 = slideShow.createSlide();
-		 slide.getSlideLayout();
+		
 		 XSLFTable table = slide.createTable();
+		 table.setAnchor(new Rectangle(50,50,500,20));
+		 for(int i=0;i<10;i++) {
 		 XSLFTableRow  row1 = table.addRow();
+		 for(int j=0;j<5;j++) {
 		  XSLFTableCell th = row1.addCell();
 	        XSLFTextParagraph p = th.addNewTextParagraph();
 	        p.setTextAlign(TextAlign.CENTER);
 	        XSLFTextRun r = p.addNewTextRun();
-	        r.setText("time ");
+	        r.setText("time_"+i+"_"+j);
 	        r.setFontSize((double) 20);
 	        r.setFontColor(new java.awt.Color(0, 0, 0));
 	        th.setFillColor(new java.awt.Color(79, 129, 189));
-	        table.setColumnWidth(0, 150);
+		 	}
+		 }
+	        
 		 
-		
-		 
+		   
 		 FileOutputStream fos = new FileOutputStream("C:\\Users\\LENOVO\\Pictures\\powerPoint\\test1.pptx");
 		 System.out.println("slide created");
 		 
          slideShow.write(fos);
          fos.close();
-         slideShow.close();
-         fis.close();
+//         slideShow.close();
+//         fis.close();
 		 
 	 }
 
